987cc0c9e35e6cdb5dd1dc1a97988dc50da19068
nanditha213 <121084004+nanditha213@users.noreply.github.com>
2024-04-15 22:29:58 +0530

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -1,123 +1,174 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+//Design a lexical analyzer for a given language and the lexical analyzer should ignore redundant spaces, tabs, and new lines. It should also ignore comments. Although the syntax specification states that identifiers can be arbitrarily long, you may restrict the length to some reasonable value. Simulate the same in C language.
 
-#define MAX_EXPR_LEN 100
-#define MAX_STACK_SIZE 50
-#define MAX_CODE_SIZE 50
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
 
-typedef struct {
-    char op;
-    char arg1[10];
-    char arg2[10];
-    char result[10];
-} Quadruple;
+#define MAX_IDENTIFIER_LENGTH 50
 
-typedef struct {
-    char op;
-    char arg1[10];
-    char arg2[10];
-} Triple;
+enum TokenType {
+    TOKEN_IDENTIFIER,
+    TOKEN_NUMBER,
+    TOKEN_OPERATOR,
+    TOKEN_KEYWORD,
+    TOKEN_SYMBOL,
+    TOKEN_UNKNOWN,
+    TOKEN_EOF
+};
 
-typedef struct {
-    char op;
-    int tripleIndex;
-} IndirectTriple;
+struct Token {
+    enum TokenType type;
+    char lexeme[MAX_IDENTIFIER_LENGTH];
+};
 
-void generateCode(char expr[]);
-void printQuadruples(Quadruple quadruples[], int count);
-void printTriples(Triple triples[], int count);
-void printIndirectTriples(IndirectTriple indirectTriples[], int count);
+// Function to check if a character is a valid identifier character
+bool is_valid_identifier_char(char c) {
+    return isalnum(c) || c == '_';
+}
+
+// Function to check if a character is a whitespace character
+bool is_whitespace(char c) {
+    return c == ' ' || c == '\t' || c == '\n' || c == '\r';
+}
+
+// Function to check if a string is a keyword
+bool is_keyword(const char *lexeme) {
+    // Add your keyword checking logic here
+    // For simplicity, let's assume some keywords
+    const char *keywords[] = {"if", "else", "while", "for", "int", "float", "return"};
+    for (int i = 0; i < sizeof(keywords) / sizeof(keywords[0]); ++i) {
+        if (strcmp(lexeme, keywords[i]) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Function to get the next token from input
+struct Token get_next_token() {
+    struct Token token;
+    char c;
+    int i = 0;
+
+    // Ignore whitespaces and comments
+    do {
+        c = getchar();
+        if (c == '/') {
+            char next_char = getchar();
+            if (next_char == '/') {
+                // Comment, skip until end of line
+                while (c != '\n' && c != EOF) {
+                    c = getchar();
+                }
+            } else {
+                // Not a comment, put back the character
+                ungetc(next_char, stdin);
+            }
+        }
+    } while (is_whitespace(c));
+
+    if (c == EOF) {
+        token.type = TOKEN_EOF;
+        return token;
+    }
+
+    // Identifiers or Keywords
+    if (is_valid_identifier_char(c)) {
+        token.type = TOKEN_IDENTIFIER;
+        token.lexeme[i++] = c;
+        while ((c = getchar()) != EOF && is_valid_identifier_char(c) && i < MAX_IDENTIFIER_LENGTH - 1) {
+            token.lexeme[i++] = c;
+        }
+        token.lexeme[i] = '\0';
+        ungetc(c, stdin);
+        if (is_keyword(token.lexeme)) {
+            token.type = TOKEN_KEYWORD;
+        }
+        return token;
+    }
+
+    // Numbers
+    if (isdigit(c)) {
+        token.type = TOKEN_NUMBER;
+        token.lexeme[i++] = c;
+        while ((c = getchar()) != EOF && (isdigit(c) || c == '.') && i < MAX_IDENTIFIER_LENGTH - 1) {
+            token.lexeme[i++] = c;
+        }
+        token.lexeme[i] = '\0';
+        ungetc(c, stdin);
+        return token;
+    }
+
+    // Operators
+    // Assuming operators are single characters
+    if (c == '+' || c == '-' || c == '*' || c == '/' || c == '=' || c == '<' || c == '>') {
+        token.type = TOKEN_OPERATOR;
+        token.lexeme[i++] = c;
+        token.lexeme[i] = '\0';
+        return token;
+    }
+
+    // Symbols
+    // Assuming symbols are single characters
+    if (c == '(' || c == ')' || c == '{' || c == '}' || c == ';' || c == ',') {
+        token.type = TOKEN_SYMBOL;
+        token.lexeme[i++] = c;
+        token.lexeme[i] = '\0';
+        return token;
+    }
+
+    // Unknown token
+    token.type = TOKEN_UNKNOWN;
+    token.lexeme[0] = c;
+    token.lexeme[1] = '\0';
+    return token;
+}
 
 int main() {
-    char expression[MAX_EXPR_LEN];
-    printf("Enter an arithmetic expression: ");
-    fgets(expression, sizeof(expression), stdin);
-    generateCode(expression);
+    struct Token token;
+
+    // Get tokens until EOF
+    do {
+        token = get_next_token();
+        switch (token.type) {
+            case TOKEN_IDENTIFIER:
+                printf("Identifier: %s\n", token.lexeme);
+                break;
+            case TOKEN_NUMBER:
+                printf("Number: %s\n", token.lexeme);
+                break;
+            case TOKEN_OPERATOR:
+                printf("Operator: %s\n", token.lexeme);
+                break;
+            case TOKEN_KEYWORD:
+                printf("Keyword: %s\n", token.lexeme);
+                break;
+            case TOKEN_SYMBOL:
+                printf("Symbol: %s\n", token.lexeme);
+                break;
+            case TOKEN_UNKNOWN:
+                printf("Unknown: %s\n", token.lexeme);
+                break;
+            case TOKEN_EOF:
+                printf("End of file\n");
+                break;
+            default:
+                break;
+        }
+    } while (token.type != TOKEN_EOF);
+
     return 0;
 }
 
-void generateCode(char expr[]) {
-    Quadruple quadruples[MAX_CODE_SIZE];
-    Triple triples[MAX_CODE_SIZE];
-    IndirectTriple indirectTriples[MAX_CODE_SIZE];
-
-    char opStack[MAX_STACK_SIZE];
-    char argStack[MAX_STACK_SIZE][10];
-
-    int opTop = -1;
-    int argTop = -1;
-
-    int tripleCount = 0;
-    int indirectTripleCount = 0;
-    int quadrupleCount = 0;
-
-    char *token = strtok(expr, " \t\n");
-    while(token != NULL) {
-        if(strcmp(token, "(") == 0) {
-            opStack[++opTop] = *token;
-        } else if(strcmp(token, ")") == 0) {
-            while(opTop != -1 && opStack[opTop] != '(') {
-                triples[tripleCount].op = opStack[opTop--];
-                strcpy(triples[tripleCount].arg1, argStack[argTop--]);
-                strcpy(triples[tripleCount].arg2, argStack[argTop--]);
-                indirectTriples[indirectTripleCount].op = triples[tripleCount].op;
-                indirectTriples[indirectTripleCount++].tripleIndex = tripleCount;
-                tripleCount++;
-            }
-            if(opTop != -1)
-                opTop--;
-        } else if(strcmp(token, "+") == 0 || strcmp(token, "-") == 0 || strcmp(token, "*") == 0 || strcmp(token, "/") == 0) {
-            opStack[++opTop] = *token;
-        } else {
-            strcpy(argStack[++argTop], token);
-        }
-        token = strtok(NULL, " \t\n");
-    }
-
-    while(opTop != -1) {
-        triples[tripleCount].op = opStack[opTop--];
-        strcpy(triples[tripleCount].arg1, argStack[argTop--]);
-        strcpy(triples[tripleCount].arg2, argStack[argTop--]);
-        indirectTriples[indirectTripleCount].op = triples[tripleCount].op;
-        indirectTriples[indirectTripleCount++].tripleIndex = tripleCount;
-        tripleCount++;
-    }
-
-    for(int i = 0; i < tripleCount; i++) {
-        strcpy(quadruples[quadrupleCount].result, triples[i].arg1);
-        strcpy(quadruples[quadrupleCount].arg1, triples[i].arg2);
-        strcpy(quadruples[quadrupleCount].arg2, "_");
-        quadruples[quadrupleCount++].op = triples[i].op;
-    }
-
-    printf("Three Address Code:\n");
-    printQuadruples(quadruples, quadrupleCount);
-    printf("\nTriples:\n");
-    printTriples(triples, tripleCount);
-    printf("\nIndirect Triples:\n");
-    printIndirectTriples(indirectTriples, indirectTripleCount);
-}
-
-void printQuadruples(Quadruple quadruples[], int count) {
-    for(int i = 0; i < count; i++) {
-        printf("%s = %s %c %s\n", quadruples[i].result, quadruples[i].arg1, quadruples[i].op, quadruples[i].arg2);
-    }
-}
-
-void printTriples(Triple triples[], int count) {
-    for(int i = 0; i < count; i++) {
-        printf("(%s, %s, %c)\n", triples[i].arg1, triples[i].arg2, triples[i].op);
-    }
-}
-
-void printIndirectTriples(IndirectTriple indirectTriples[], int count) {
-    for(int i = 0; i < count; i++) {
-        printf("(%d, %c)\n", indirectTriples[i].tripleIndex, indirectTriples[i].op);
-    }
-}
-
-// INPUT
-
-
-// (a + b) * (c - d)
+// INPUT : 
+// int main() {
+//     int x = 10;
+//     float y = 20.5;
+//     if (x > 5) {
+//         printf("x is greater than 5\n");
+//     } else {
+//         printf("x is less than or equal to 5\n");
+//     }
+//     return 0;
+// }
